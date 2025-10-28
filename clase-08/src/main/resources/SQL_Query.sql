ada8303c39cd6f08bff30ad2ca2e117a82bfb166
Alejandro Di Stefano <addistefano@live.com.ar>
2025-10-28 09:53:01 -0300

    Clase 09

---
+++ clase-08/src/main/resources/SQL_Query.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS java_coderhouse_2;
+USE  java_coderhouse_2;
+
+SELECT * FROM alumnos;
+SELECT * FROM cursos;
+SELECT * FROM curso_alumno;
+SELECT * FROM categorias;
+
+
+SELECT * FROM cursos WHERE id = 1;
